826c2cb48cadc75b9c802f65b52277d675db6db8
sergiotravassos <sergiotravassos@outlook.com>
2022-09-02 11:47:56 +0100

    project created

---
+++ backend/src/main/java/com/sergiotravassos/learnplatform/LearnplatformApplication.java
@@ -0,0 +1,13 @@
+package com.sergiotravassos.learnplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnplatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sergiotravassos/learnplatform/LearnplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiotravassos.learnplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
