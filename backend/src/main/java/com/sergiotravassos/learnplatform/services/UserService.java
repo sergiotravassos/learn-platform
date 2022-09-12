9c11bc54d89df17cee5bbac121e713f8e4b2996e
sergiotravassos <sergiotravassos@outlook.com>
2022-09-12 11:56:08 +0100

    Validation and Security

---
+++ backend/src/main/java/com/sergiotravassos/learnplatform/services/UserService.java
@@ -0,0 +1,32 @@
+package com.sergiotravassos.learnplatform.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.sergiotravassos.learnplatform.entities.User;
+import com.sergiotravassos.learnplatform.repositories.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService{
+
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(username);
+		if(user == null) {
+			logger.error("User not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("User found: " + username);
+		return user;
+	}
+}
